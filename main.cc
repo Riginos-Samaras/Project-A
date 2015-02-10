9b0b1e4a126db975f0db850fe83c4c94e6bf0131
dimitriosboglou <dimitriosboglou@192.168.0.101>
2015-02-10 21:11:00 +0200

    Riginos: until nodeGraph

---
+++ main.cc
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2009-2010, Oracle and/or its affiliates. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of Oracle nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "node.h" 
+#include "nodeGraph.h" 
+
+using namespace::std;
+
+int main(int argc, char**argv) {
+    // Prints welcome message...
+    std::cout << "Welcome ..." << std::endl;
+    //node x(1,1); 
+    //nodeGraph Graph();
+    node x1(1,"Riginos");
+    node x2(2,"Michael"); 
+    //Graph.insertNode(1,2);
+    x1.insertInputNode(&x2); 
+    
+    std::cout<<x1.getName()<<x1.inNodes.front()->getName();
+    //x.in_nodes.push_back(10);
+    //x.in_nodes.push_back(5);
+    //std::cout<<x.in_nodes.back();
+    // Prints arguments...
+//    std::cout<<x.get_name() <<x.get_value()<< x.is_done();
+   // x.insert_input_node(1,1)
+    if (argc > 1) {
+        std::cout << std::endl << "Arguments:" << std::endl;
+        for (int i = 1; i < argc; i++) {
+            std::cout << i << ": " << argv[i] << std::endl;
+        }
+    }
+    
+    return 0;
+}
